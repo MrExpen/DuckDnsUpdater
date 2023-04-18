036f2e77e412b1a1150ae4dd23ca1b57e3e8a706
Dmitrii Chibrikov <chi-dimochka@yandex.ru>
2023-04-18 15:12:12 +0400

    Initial commit

---
+++ DuckDnsUpdater/Program.cs
@@ -0,0 +1,29 @@
+﻿string domain = Environment.GetEnvironmentVariable("DOMAIN");
+if (string.IsNullOrWhiteSpace(domain))
+{
+    throw new ArgumentException(nameof(domain));
+}
+
+string token = Environment.GetEnvironmentVariable("TOKEN");
+if (string.IsNullOrWhiteSpace(token))
+{
+    throw new ArgumentException(nameof(token));
+}
+
+while (true)
+{
+    var response =
+        await new HttpClient()
+            .GetStringAsync($"https://www.duckdns.org/update?domains={domain}&token={token}");
+
+    if (response == "OK")
+    {
+        Console.WriteLine("duckdns update ok");
+    }
+    else
+    {
+        Console.WriteLine("duckdns update failed");
+    }
+
+    await Task.Delay(TimeSpan.FromMinutes(5));
+}
