c03458ec07924519c8e66a701b976168dfd7eb85
Dmitrii Chibrikov <chi-dimochka@yandex.ru>
2023-11-03 00:44:57 +0400

    added HttpClientCacher

---
+++ DuckDnsUpdater/HttpClientCacher.cs
@@ -0,0 +1,39 @@
+﻿namespace DuckDnsUpdater;
+
+public class HttpClientCacher : IDisposable
+{
+    private HttpClient? _httpClient;
+
+    private readonly TimeSpan _validInterval;
+
+    private DateTime ValidBefore { get; set; } = DateTime.MinValue;
+
+    public HttpClient HttpClient
+    {
+        get
+        {
+            if (DateTime.Now > ValidBefore || _httpClient is null)
+            {
+                _httpClient?.Dispose();
+                _httpClient = new HttpClient();
+                ValidBefore = DateTime.Now + _validInterval;
+            }
+
+            return _httpClient;
+        }
+    }
+
+    public HttpClientCacher(TimeSpan validInterval)
+    {
+        _validInterval = validInterval;
+    }
+
+    public HttpClientCacher() : this(TimeSpan.FromHours(1))
+    {
+    }
+
+    public void Dispose()
+    {
+        _httpClient?.Dispose();
+    }
+}
